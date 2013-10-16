488742d3480e8979ed6b8a4374c53ce7f70a8da7
Chris A. Taylor <chris@gameclosure.com>
2013-10-15 18:19:43 -0700

    Thank you for subscribing to curve-facts

--- snowshoe/Snowshoe.cpp
+++ snowshoe/Snowshoe.cpp
@@ -719,6 +719,12 @@ static void ted_split(const u64 m[4], leg &a, leg &b) {
  * Extended Twisted Edwards Group Laws [5]
  *
  * Curve: a*u*x^2 + y^2 = d*u*x^2*y^2 /Fp^2, p=2^127-1, a=-1, d=109, u=2+i
+ *
+ * (0, 1) is the identity element
+ * (0, -1) is of order 2
+ * This codebase avoids x=0 entirely.
+ *
+ * -(x,y) = (-x, y)
  */
 
 static const u32 TED_D = 109;
